cf00b4c06a3b32b38c3168ae02af78275a90737f
swos <szymon.wos1319@gmail.com>
2024-09-14 14:48:08 +0200

    feat: fetch users from API

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+import type { User } from "./types";
+
+export default async function getUsers(): Promise<User[]> {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.log(error.message));
+  //TODO: handle errors
+  return data;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { Typography } from "@mui/material";
+import getUsers from "../api";
+import type { User } from "../types";
 
-export default function Home() {
-  return <Typography variant="h1">User management table</Typography>;
+export default async function Home() {
+  const users: User[] = await getUsers();
+  console.log("Console from home", users);
+
+  return (
+    <>
+      <Typography variant="h1">User management table</Typography>
+    </>
+  );
 }
